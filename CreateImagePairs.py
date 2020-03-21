Fixed SOS and scaling

CreateImagePairs.py
@@ -82,10 +82,11 @@ def trans_motion(input, dir_motion, maxshift, sigma_shift, prob):
             num_x, num_y = input.shape[1:]
             startdir = np.random.randint(0,1)   # Always start in PE (column)
 
-            shift_bulk0 = (2*np.random.randint(0,2)-1) * np.random.normal(maxshift, sigma_shift)        # shift in x
-            shift_bulk1 = (2*np.random.randint(0,2)-1) * np.random.normal(maxshift, sigma_shift)        # shift in y
-            # shift_bulk0 = 0
-            # shift_bulk1 = 30
+            shift_bulk0 = (2 * np.random.randint(0, 2) - 1) * np.random.randint(1, maxshift)  # shift in x
+            shift_bulk1 = (2 * np.random.randint(0, 2) - 1) * np.random.randint(1, maxshift)  # shift in y
+            # shift_bulk0 = (2*np.random.randint(0,2)-1) * np.random.normal(maxshift, sigma_shift)        # shift in x
+            # shift_bulk1 = (2*np.random.randint(0,2)-1) * np.random.normal(maxshift, sigma_shift)        # shift in y
+
 
             logger.info(f'shift in x is {shift_bulk0} pixels')
             logger.info(f'shift in y is {shift_bulk1} pixels')
@@ -282,9 +283,9 @@ def get_truth(kspace, sl, device, lamda):
     return image_truth
 
 
-def get_corrupted(kspace, sl, num_coils, num_corrupted, device, acc=0, acc_ulim=20,
-                  kedge_len=30, gaussian_ulim=18, gaussian_prob=2, dir_motion=2, maxshift=20, sigma_shift=6,
-                  motion_prob=1, incoherent_prob=2, dump=0.5, mode_incoherent=1):
+def get_corrupted(kspace, sl, num_coils, num_corrupted, device, acc=0, acc_ulim=15,
+                  kedge_len=30, gaussian_ulim=12, gaussian_prob=2, dir_motion=2, maxshift=20, sigma_shift=6,
+                  motion_prob=3, incoherent_prob=2, dump=0.7, mode_incoherent=1):
     logger = logging.getLogger('get_corrupted')
 
     # get smaps from original ksp
@@ -358,9 +359,10 @@ def get_corrupted(kspace, sl, num_coils, num_corrupted, device, acc=0, acc_ulim=
     ksp2_gpu = sp.to_device(ksp2, device=device)
 
     # RECON. 0 for sos, 1 for PILS, 2 for L2 SENSE, 3 for l1 wavelet, 4 for tv
-    recon_type = np.random.randint(5)
+    recon_type = 0
+    # recon_type = np.random.randint(5)
 
-    mu_iter, sigma_iter = 20, 3
+    mu_iter, sigma_iter = 30, 3
     maxiter = np.int(np.ceil(np.abs(np.random.normal(mu_iter, sigma_iter, 1))))
 
     if recon_type == 0:
@@ -368,8 +370,11 @@ def get_corrupted(kspace, sl, num_coils, num_corrupted, device, acc=0, acc_ulim=
         ksp2_tensor = T.to_tensor(ksp2)  # T requires array to be tensor
         image = T.ifft2(ksp2_tensor)  # torch.Size([20, 640, 320, 2])
         image = image.numpy()
-        image = image[:, :, :, 0] + 1j * image[:, :, :, 1]  # torch.Size([20, 640, 320])
-        image = np.sqrt(np.sum(image ** 2, axis=0))  # torch.Size([640, 320])
+        image = image[:, :, :, 0] + 1j * image[:, :, :, 1]  # (coil, h, w)
+        #abs
+        image = np.abs(image)
+        image = image.astype('complex128')
+        image = np.sqrt(np.sum(image ** 2, axis=0))  # (h, w)
 
     elif recon_type == 1:
         logger.info('PILS')
@@ -498,7 +503,7 @@ def generate_pairs():
                 im = Image.fromarray(255 * np.abs(image_sense))
                 im = im.convert("L")
                 name = 'NYU_%07d_TRUTH.png' % count
-                name = os.path.join('Rank_NYU', 'ImagePairs_png_v5', name)
+                name = os.path.join('Rank_NYU', 'ImagePairs_png_v6', name)
                 im.save(name)
 
                 print(f'saving slice {s} of file # {index_file + 1} to count {count}')
@@ -517,8 +522,8 @@ def generate_pairs():
                         image_corrupted1, acc1, gaussian_level1, percent1, mode1 = get_corrupted(ksp, s, num_corrupted=recon, num_coils=tot_coils, device=device)  # has been normalized to its max
                         logger.info(f'gaussian_level is {gaussian_level1} for both')
                         # Find scaling that minimizes MSE(corrupted, ori)
-                        scale = np.sum(np.conj(image_corrupted1).T @ image_sense) / np.sum(
-                            np.conj(image_corrupted1).T @ image_corrupted1)
+                        scale = np.sum(np.conj(image_corrupted1).T * image_sense) / np.sum(
+                            np.conj(image_corrupted1).T * image_corrupted1)
                         # print(f'scale for file # {index_file + 1}, slice # {s}, recon # {recon} is {scale} ')
                         # logger.info(f'scale is {scale}')
                         image_corrupted1 *= scale
@@ -529,7 +534,7 @@ def generate_pairs():
                         logger.info(f'SSIM between corrupted1 and truth = {ssim1}')
 
                         name = 'NYU_%07d_IMAGE_%04d.png' % (count, recon)
-                        name = os.path.join('Rank_NYU', 'ImagePairs_png_v5', name)
+                        name = os.path.join('Rank_NYU', 'ImagePairs_png_v6', name)
                         im = Image.fromarray(255 * np.abs(image_corrupted1))
                         im = im.convert("L")
                         im.save(name)
@@ -544,8 +549,8 @@ def generate_pairs():
                         image_corrupted, acc, gaussian_level, percent, mode = get_corrupted(ksp, s, num_corrupted=recon, num_coils=tot_coils, device=device, acc=acc1, gaussian_ulim=gaussian_level1, dump=percent1, mode_incoherent=mode1)  # has been normalized to its max
                         logger.info(f'Incoherent noise mode is {mode}. dump {1-percent} PE for addding incoherent noise')
                         # Find scaling that minimizes MSE(corrupted, ori)
-                        scale = np.sum(np.conj(image_corrupted).T @ image_sense) / np.sum(
-                            np.conj(image_corrupted).T @ image_corrupted)
+                        scale = np.sum(np.conj(image_corrupted).T * image_sense) / np.sum(
+                            np.conj(image_corrupted).T * image_corrupted)
                         # print(f'scale for file # {index_file + 1}, slice # {s}, recon # {recon} is {scale} ')
                         # logger.info(f'scale is {scale}')
                         image_corrupted *= scale
@@ -567,8 +572,8 @@ def generate_pairs():
                                                                  device=device,
                                                                  acc=acc1, gaussian_ulim=gaussian_level1, dump=percent1,
                                                                                                 mode_incoherent=mode1)  # has been normalized to its max
-                            scale = np.sum(np.conj(image_corrupted).T @ image_sense) / np.sum(
-                                np.conj(image_corrupted).T @ image_corrupted)
+                            scale = np.sum(np.conj(image_corrupted).T * image_sense) / np.sum(
+                                np.conj(image_corrupted).T * image_corrupted)
                             image_corrupted *= scale
 
                             # mse2 = compare_nrmse(image_sense, image_corrupted, norm_type='euclidean')
@@ -579,7 +584,7 @@ def generate_pairs():
                             if counter_regenerate > 12:
                                 logger.info(f'Too many tries, settle on this one. ssim12 = {np.abs(ssim2-ssim1)}')
                                 name = 'NYU_%07d_IMAGE_%04d.png' % (count, recon)
-                                name = os.path.join('Rank_NYU', 'ImagePairs_png_v5', name)
+                                name = os.path.join('Rank_NYU', 'ImagePairs_png_v6', name)
                                 im = Image.fromarray(255 * np.abs(image_corrupted))
                                 im = im.convert("L")
                                 im.save(name)
@@ -594,7 +599,7 @@ def generate_pairs():
                         else:
                             logger.info(f'SSIM between corrupted1 and 2 = {diff_ssim12}')
                             name = 'NYU_%07d_IMAGE_%04d.png' % (count, recon)
-                            name = os.path.join('Rank_NYU', 'ImagePairs_png_v5', name)
+                            name = os.path.join('Rank_NYU', 'ImagePairs_png_v6', name)
                             im = Image.fromarray(255 * np.abs(image_corrupted))
                             im = im.convert("L")
                             im.save(name)
@@ -618,8 +623,8 @@ def generate_pairs():
 
                 count += 1
 
-            # if index_file > 0:
-            #     break
+            if index_file > 4:
+                break
 
 
 if __name__ == "__main__":

rank_small_torch.py
@@ -1,4 +1,7 @@
-
+import tkinter as tk
+from tkinter import filedialog
+import fnmatch
+import os
 import numpy as np
 import h5py as h5
 import matplotlib.pyplot as plt
@@ -10,15 +13,44 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 import torch.optim as optim
-# from livelossplot import PlotLosses
-# from torch.utils.tensorboard import SummaryWriter
-# import torchsummary
+#from livelossplot import PlotLosses
+#from torch.utils.tensorboard import SummaryWriter
+import torchsummary
+
+
+def zero_padding(input, oshapex, oshapey):
+    # pad x
+    padxL = int(np.floor((oshapex - input.shape[0])/2))
+    padxR = int(oshapex - input.shape[0] - padxL)
+
+    # pad y
+    padyU = int(np.floor((oshapey - input.shape[1])/2))
+    padyD = int(oshapey - input.shape[1] - padyU)
+
+    input = np.pad(input, ((padxL, padxR),(padyU, padyD)), 'constant', constant_values=0)
+    return input
+
+
+def imshow(im):
+    npim = im.numpy()
+    npim = np.squeeze(npim)
+    abs = np.sqrt(npim[:,:,0]**2 + npim[:,:,1]**2)
+    plt.imshow(abs,cmap='gray')
+    plt.show()
+
 
 subtract_truth = True
 shuffle_observers = True
 
 # Ranks
-names = ('ranks_CT_const.csv', 'ranks_CT_const2.csv')
+names = []
+root = tk.Tk()
+filepath_csv = tk.filedialog.askdirectory(title='Choose where the csv file is')
+files_csv = os.listdir(filepath_csv)
+for file in files_csv:
+    if fnmatch.fnmatch(file, '*.csv'):
+        names.append(os.path.join(filepath_csv, file))
+
 # Load the ranks
 ranks = []
 for fname in names:
@@ -34,26 +66,32 @@ if shuffle_observers:
     np.random.shuffle(ranks)
 
 # Examples
+maxMatSize = 396    # largest matrix size seems to be 396
 NEXAMPLES = ranks.shape[0]
-X_1 = np.zeros((NEXAMPLES,256,256),dtype=np.complex64)
-X_2 = np.zeros((NEXAMPLES,256,256),dtype=np.complex64)
-X_T = np.zeros((NEXAMPLES,256,256),dtype=np.complex64)
+X_1 = np.zeros((NEXAMPLES, maxMatSize, maxMatSize),dtype=np.complex64)   # saved as complex128 though
+X_2 = np.zeros((NEXAMPLES, maxMatSize, maxMatSize),dtype=np.complex64)
+X_T = np.zeros((NEXAMPLES, maxMatSize, maxMatSize),dtype=np.complex64)
 Labels = np.zeros(NEXAMPLES, dtype=np.int32)
 
-hf = h5.File(name='TRAINING_IMAGES_V1.h5', mode='r')
+root = tk.Tk()
+filepath_images = tk.filedialog.askdirectory(title='Choose where the h5 is')
+for file in os.listdir(filepath_images):
+    if fnmatch.fnmatch(file, '*.h5'):
+        file_images = os.path.join(filepath_images, file)
+hf = h5.File(name=file_images, mode='r')
 
 # Make a real database of images
 for i in range(0, ranks.shape[0]):
     # print('Example %d, Image %d ( %d %d)' % (i, ranks[i, 2], ranks[i, 0], ranks[i, 1]))
 
-    name = 'EXAMPLE_%06d_TRUTH' % (ranks[i,2])
-    X_T[i, :, :] = np.array(hf[name])
+    name = 'EXAMPLE_%07d_TRUTH' % (ranks[i,2])
+    X_T[i, :, :] = zero_padding(np.array(hf[name]), maxMatSize, maxMatSize)
 
-    name = 'EXAMPLE_%06d_IMAGE_%04d' % (ranks[i, 2], 0)
-    X_1[i, :, :] = np.array(hf[name])
+    name = 'EXAMPLE_%07d_IMAGE_%04d' % (ranks[i, 2], 0)
+    X_1[i, :, :] = zero_padding(np.array(hf[name]), maxMatSize, maxMatSize)
 
-    name = 'EXAMPLE_%06d_IMAGE_%04d' % (ranks[i, 2], 1)
-    X_2[i, :, :] = np.array(hf[name])
+    name = 'EXAMPLE_%07d_IMAGE_%04d' % (ranks[i, 2], 1)
+    X_2[i, :, :] = zero_padding(np.array(hf[name]), maxMatSize, maxMatSize)
 
     # Label based on ranks from ranker
     if ranks[i,0] == 2:
@@ -89,9 +127,8 @@ X_1_cnnT = np.transpose(X_1_cnnT,[0,3,1,2])
 X_2_cnnT = np.transpose(X_2_cnnT,[0,3,1,2])
 X_1_cnnV = np.transpose(X_1_cnnV,[0,3,1,2])
 X_2_cnnV = np.transpose(X_2_cnnV,[0,3,1,2])
-print(X_1_cnnT.shape)
-
-# Transform
+print(f'Training set size {X_1_cnnT.shape}')
+print(f'Validation set size {X_1_cnnV.shape}')
 
 
 # Data generator
@@ -133,13 +170,6 @@ checkim1 = checkim1.permute(0,2,3,1)
 checkim2 = checkim2.permute(0,2,3,1)
 checklbnp = checklb.numpy()
 
-def imshow(im):
-    npim = im.numpy()
-    npim = np.squeeze(npim)
-    abs = np.sqrt(npim[:,:,0]**2 + npim[:,:,1]**2)
-    plt.imshow(abs,cmap='gray')
-    plt.show()
-
 randnum = np.random.randint(16)
 imshow(checkim1[randnum,:,:,:])
 imshow(checkim2[randnum,:,:,:])
@@ -151,7 +181,7 @@ print(f'Label is {checklbnp[randnum]}')
 channels_cnn = 16
 kernel_size = 3
 stride_size = 1
-image_size = 256
+image_size = maxMatSize
 padding_size = 1
 
 
@@ -231,7 +261,7 @@ class Ranknet(nn.Module):
     #         num_features *= s
     #     return num_features
 ranknet = Ranknet()
-torchsummary.summary(ranknet, (2,256,256), device="cpu")
+torchsummary.summary(ranknet, (2,maxMatSize, maxMatSize), device="cpu")
 
 class Classifier(nn.Module):
 
@@ -303,10 +333,10 @@ class RunningAverage:
         return self.sum / self.count
 
 
-liveloss = PlotLosses()
+# liveloss = PlotLosses()
 
 # Training
-for epoch in range(100):
+for epoch in range(10):
 
     logs = {}
     # Setup counter to keep track of loss
@@ -348,15 +378,6 @@ for epoch in range(100):
 
 
         running_loss += loss.item()
-        # if i % 10 == 9:  # every 10 mini-batches...
-        #
-        #     # ...log the running loss
-        #     writer.add_scalar('training loss',
-        #                       running_loss / 10,
-        #                       epoch * len(loader_T) + i)
-        #
-        #     # ...log a Matplotlib Figure showing the model's predictions on a
-        #     # random mini-batch
 
 
     # validation
@@ -377,12 +398,13 @@ for epoch in range(100):
 
         print(f'validation loss is {loss}')
 
+    print(f'training average loss is {train_avg.avg()}')
+    print(f'training average loss is {eval_avg.avg()}')
+    # logs['loss'] = train_avg.avg()
+    # logs['val_loss'] = eval_avg.avg()
 
-    logs['loss'] = train_avg.avg()
-    logs['val_loss'] = eval_avg.avg()
-
-    liveloss.update(logs)
-    liveloss.draw()
+    # liveloss.update(logs)
+    # liveloss.draw()
 
     print('Epoch = %d : Loss Eval = %f , Loss Train = %f' % (epoch, eval_avg.avg(), train_avg.avg()))
 

test_corruptedH5.py
@@ -18,22 +18,30 @@ root = tk.Tk()
 root.withdraw()
 filepath = tk.filedialog.askdirectory(title='Select NYU.h5 folder')
 os.chdir(filepath)
-file = os.path.join(filepath, 'TRAINING_IMAGES_v5.h5')
+file = os.path.join(filepath, 'UTE_REF_sl13_enc14.h5')
 
 hf = h5py.File(name=file, mode='r')
 
 # Make a real database of images
-X_T = np.zeros((8,320,320),dtype=np.complex128)
-X_0 = np.zeros((8,320,320),dtype=np.complex128)
-X_1 = np.zeros((8,320,320),dtype=np.complex128)
-for i in range(0, 8):
+# X_T = np.zeros((885,320,320),dtype=np.complex128)
+# X_0 = np.zeros((885,320,320),dtype=np.complex128)
+# X_1 = np.zeros((885,320,320),dtype=np.complex128)
+# for i in range(0, 885):
 
-    name = 'EXAMPLE_%07d_TRUTH' % (i+1)
-    X_T[i, :, :] = np.array(hf[name])
+    # name = 'EXAMPLE_%07d_TRUTH' % (i+1)
+    # X_T[i, :, :] = np.array(hf[name])
 
-    name = 'EXAMPLE_%07d_IMAGE_%04d' % (i + 1, 0)   # 0000.png
-    X_0[i, :, :] = np.array(hf[name])
+    # name = 'EXAMPLE_%07d_IMAGE_%04d' % (i + 1, 0)   # 0000.png
+    # X_0[i, :, :] = np.array(hf[name])
+    #
+    # name = 'EXAMPLE_%07d_IMAGE_%04d' % (i + 1, 1)   # 0001.png
+    # X_1[i, :, :] = np.array(hf[name])
 
-    name = 'EXAMPLE_%07d_IMAGE_%04d' % (i + 1, 1)   # 0001.png
-    X_1[i, :, :] = np.array(hf[name])
+asl_ref = np.zeros((256,256),dtype=np.complex64)
+ute = np.zeros((256,256),dtype=np.complex64)
 
+name = 'REF'
+asl_ref = np.array(hf[name])
+
+name = 'UTE'
+ute = np.array(hf[name])