Fix backpropogation across sqrt

rank_small_torch.py
@@ -52,7 +52,7 @@ if Pretrain == 'pretraining':
 
 else:
     names = []
-    filepath_csv = Path('V:\LearnedImageMetric\ImagePairs_Pack_04032020')
+    filepath_csv = Path('E:\LearnedImageMetric\ImagePairs_Pack_04032020')
     os.chdir(filepath_csv)
 
     files_csv = os.listdir(filepath_csv)
@@ -99,7 +99,7 @@ Labels = np.zeros(NRANKS, dtype=np.int32)
 
 if Pretrain == 'pretraining':
     filepath_images = Path("I:\code\LearnedImagingMetrics_pytorch\Rank_NYU\ImagePairs_Pack_05062020")
-    path2 = Path("I:\code\LearnedImagingMetrics_pytorch\Rank_NYU\ImagePairs_Pack_05072020")
+    path2 = Path("I:\code\LearnedImagingMetrics_pytorch\Rank_NYU\ImagePairs_Pack_05062020")
     file = 'TRAINING_IMAGES_v7.h5'
     file1 = os.path.join(filepath_images, file)
     file2 = os.path.join(path2, file)
@@ -140,7 +140,7 @@ if Pretrain == 'pretraining':
 
 
 else:
-    filepath_images = Path('V:\LearnedImageMetric\ImagePairs_Pack_04032020')
+    filepath_images = Path('E:\LearnedImageMetric\ImagePairs_Pack_04032020')
     file ='TRAINING_IMAGES_04032020.h5'
     file_images = os.path.join(filepath_images, file)
     hf = h5.File(name=file_images, mode='r')
@@ -300,7 +300,7 @@ def train_evaluate(parameterization):
 
 if ResumeTrain:
     # load RankNet
-    filepath_rankModel = Path('V:\LearnedImageMetric')
+    filepath_rankModel = Path('E:\LearnedImageMetric')
     file_rankModel = os.path.join(filepath_rankModel, "RankClassifier8085_pretraining.pt")
     classifier = Classifier(ranknet)
     #classifier.rank.register_backward_hook(printgradnorm)

utils/Recon_helper.py
@@ -274,11 +274,12 @@ def loss_fcn_onenet(noisy, output, target, projector, encoder, discriminator, di
 
 
 # learned metrics loss
-def learnedloss_fcn(output, target, scoreModel, rank_trained_on_mag=True):
+def learnedloss_fcn(output, target, scoreModel, rank_trained_on_mag=False):
 
     if output.ndim == 3:
         output = torch.unsqueeze(output, 0)
         target = torch.unsqueeze(target, 0)
+
     if rank_trained_on_mag and output.shape[-1] == 2:
         output = torch.sqrt(torch.sum(output**2, axis=-1, keepdims=True))
 
@@ -289,13 +290,13 @@ def learnedloss_fcn(output, target, scoreModel, rank_trained_on_mag=True):
 
     Nslice = output.shape[0]
 
-    if not rank_trained_on_mag:
-        # add a zero channel since ranknet expect 3chan
-        zeros = torch.zeros(((Nslice, 1,) + output.shape[2:]), dtype=output.dtype)
-        zeros = zeros.cuda()
-        output = torch.cat((output, zeros), dim=1)
 
-        target = torch.cat((target, zeros), dim=1)      # (batch=1, 3, 396, 396)
+    # if not rank_trained_on_mag:
+    #     # add a zero channel since ranknet expect 3chan
+    #     zeros = torch.zeros(((Nslice, 1,) + output.shape[2:]), dtype=output.dtype)
+    #     zeros = zeros.cuda()
+    #     output = torch.cat((output, zeros), dim=1)
+    #     target = torch.cat((target, zeros), dim=1)      # (batch=1, 3, 396, 396)
 
     delta = 0
     for sl in range(Nslice):

utils/model_helper.py
@@ -191,16 +191,19 @@ class L2cnn(nn.Module):
 
         diff = input - truth
         # if train on 2chan (real and imag) images
-        diff = torch.sqrt(diff[:,0,:,:]**2+diff[:,1,:,:]**2)
-        diff = diff.unsqueeze_(1)
+        #diff_mag  = torch.sqrt(diff[:,0,:,:]**2+diff[:,1,:,:]**2)
+        diff_sq = torch.square(diff)
+        diff_sq = torch.sum( diff_sq, dim=1)
+        diff_mag = diff_sq ** (0.5)
+        diff_mag = diff_mag.unsqueeze_(1)
 
         # Mean square error
-        mse = self.layer_mse(diff)
+        mse = self.layer_mse(diff_mag)
 
         # Convolutional pathway
         for l in self.layers:
-            diff = l(diff)
-        cnn_score = self.layer_mse(diff)
+            diff_mag = l(diff_mag)
+        cnn_score = self.layer_mse(diff_mag)
 
         #x = torch.reshape(x,(x.shape[0],-1))
         #x = x**2